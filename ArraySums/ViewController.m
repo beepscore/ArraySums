314839a935fe626edfadea7bb0e0cf42d6b42293
beepscore <support@beepscore.com>
2016-01-19 08:17:45 -0800

    Initial Commit

---
+++ ArraySums/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  ArraySums
+//
+//  Created by Steve Baker on 1/19/16.
+//  Copyright © 2016 Beepscore LLC. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
